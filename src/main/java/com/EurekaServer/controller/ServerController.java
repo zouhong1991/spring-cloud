5fcef51d18f75b41c334b8871694916561f61520
wudiyong <774695423@qq.com>
2017-11-26 16:09:51 +0800

    添加日志配置文件

---
+++ src/main/java/com/EurekaServer/controller/ServerController.java
@@ -0,0 +1,25 @@
+package com.EurekaServer.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.EurekaServer.entity.UserPojo;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+@RestController
+public class ServerController {
+	Logger log = LoggerFactory.getLogger(ServerController.class);
+	
+	//发起POST请求时，需要设置报文头部Content-Type=application/json，否则会报415错误
+	@RequestMapping(value = "/testPost",method = RequestMethod.POST)
+	public UserPojo testPost(HttpServletResponse response,HttpServletRequest request,@RequestBody UserPojo userPojo) {
+		log.info("name:{},age:{}",userPojo.getName(),userPojo.getAge());
+		return userPojo;
+	}
+}
